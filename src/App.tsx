e6135e6fe8b8cd22c2dac8d4e09b39e69914bb10
George <gjkoutroumpis@gmail.com>
2023-04-19 22:55:54 -0400

    Added state to GameForm component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import GameForm from "./components/GameForm";
 import CardList from "./components/CardList";
+import { useState } from "react";
 
 const App = () => {
-  let games: Game[] = [
+
+  const [games, setGames] = useState<Game[]>([
     {
       "title": "TimeSplitters 2",
       "platform": "Nintendo Gamecube"
@@ -11,10 +13,14 @@ const App = () => {
       "title": "F-Zero GX",
       "platform": "Nintendo Gamecube"
     }
-  ]
+  ])
+  const onSubmitForm = (game: Game) => {
+    setGames([...games, game])
+    debugger
+  }
   return (
     <div className="container">
-      <GameForm />
+      <GameForm onSubmitForm={onSubmitForm}/>
       <CardList games={games} />
     </div>
   );
--- src/components/GameForm.tsx
+++ src/components/GameForm.tsx
@@ -1,19 +1,68 @@
+import { useState } from "react";
 import { Button, Form } from "react-bootstrap";
+import GameCard from "./GameCard";
 
-const GameForm = () => {
+interface Props {
+  onSubmitForm: (game: Game) => void;
+}
+
+const GameForm = ({ onSubmitForm }: Props) => {
+  const [gameValues, setGameValues] = useState({
+    title: "",
+    platform: "",
+  });
+
+  const handleInputChange = (e: any) => {
+    const { name, value } = e.target;
+    setGameValues((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmitForm = () => {
+    if (gameValues.title.length === 0 || gameValues.platform.length === 0) {
+      return;
+    }
+    let game: Game = {
+      title: gameValues.title,
+      platform: gameValues.platform,
+    };
+    onSubmitForm(game);
+  };
+
+  const inputClass = gameValues.platform.length;
   return (
     <>
       <h1>Video Game Form</h1>
       <Form>
         <Form.Group className="mb-3" controlId="formTitle">
           <Form.Label>Title</Form.Label>
-          <Form.Control type="text" placeholder="Enter Game Title " />
+          <Form.Control
+            type="text"
+            name="title"
+            value={gameValues.title}
+            placeholder="Enter Game Title "
+            onChange={handleInputChange}
+          />
+          <Form.Control.Feedback type="invalid">
+            This field is required.
+          </Form.Control.Feedback>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formPlatform">
           <Form.Label>Platform</Form.Label>
-          <Form.Control type="input" placeholder="Enter Game Platform" />
+          <Form.Control
+            type="text"
+            name="platform"
+            placeholder="Enter Game Platform"
+            value={gameValues.platform}
+            onChange={handleInputChange}
+          />
+          <Form.Control.Feedback type="invalid">
+            This field is required.
+          </Form.Control.Feedback>
         </Form.Group>
-        <Button variant="primary" type="submit">
+        <Button variant="primary" onClick={handleSubmitForm}>
           Submit
         </Button>
       </Form>
